e50ed85ea541b431b33bc663098cc447813370b6
BHUVESH ROHANKAR <bhuveshrohankar28@gmail.com>
2026-02-20 22:08:10 +0000

    Sync LeetCode submission Runtime - 4 ms (3.36%), Memory - 46.9 MB (53.53%)

---
+++ 0412-fizz-buzz/solution.java
@@ -0,0 +1,21 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        List<String> answer = new ArrayList<>();
+        for (int i = 1; i <=n; i++) {
+            String currentStr="";
+            if (i%3==0) currentStr+="Fizz";
+            if (i%5==0) currentStr+="Buzz";
+            if (currentStr.isEmpty()) {currentStr+=Integer.toString(i);
+                
+            }answer.add(currentStr);
+            
+            
+        }
+return answer;
+        }
+    }
+
