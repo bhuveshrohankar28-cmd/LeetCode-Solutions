97117e4d5379f1c90cfb315501cb9fcc9e1ac886
BHUVESH ROHANKAR <bhuveshrohankar28@gmail.com>
2026-02-22 22:23:07 +0000

    Add solution Runtime - 4 ms (82.37%), Memory - 46.8 MB (9.18%)

--- 0242-valid-anagram/solution.java
+++ 0242-valid-anagram/solution.java
@@ -1,21 +1,16 @@
+
+import java.util.Arrays;
+
 class Solution {
     public boolean isAnagram(String s, String t) {
         if (s.length()!=t.length()) {
             return false;
-        } 
-    HashMap<Character,Integer> aMap=new HashMap<>();
-    for (char ele : s.toCharArray()) {
-        aMap.put(ele, aMap.getOrDefault(ele, 0) + 1);
-    }
-    for (char ele : t.toCharArray()) {
-        if (!aMap.containsKey(ele)) {
-            return false;
-        }if(aMap.get(ele)==0) return false;
-        aMap.put(ele, aMap.get(ele) - 1);
-        // if (aMap.get(ele) < 0) {
-        //     return false;
-        // }
-    }
-    return true;
+        }
+       char[] sChars=s.toCharArray();
+       char[] tChars=t.toCharArray();
+        Arrays.sort(sChars);
+        Arrays.sort(tChars);
+        return Arrays.equals(sChars,tChars);
+
     }
 }
