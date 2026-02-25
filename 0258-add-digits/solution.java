c8dba926e6165e4ecd588c2ba25732b53b34e70a
BHUVESH ROHANKAR <bhuveshrohankar28@gmail.com>
2026-02-25 21:51:07 +0000

    Add solution Runtime - 1 ms (97.89%), Memory - 42.8 MB (25.63%)

--- 0258-add-digits/solution.java
+++ 0258-add-digits/solution.java
@@ -1,7 +1,18 @@
 class Solution {
     public int addDigits(int num) {
-        if(num==0)return 0;
-        if(num%9==0)return 9;
-        return num%9;
+         int sum=0;
+        while(num>=10)
+        {
+            sum=0;
+            while(num>0)
+            {
+                int temp= num%10;
+                sum+=temp;
+                num/=10;
+            }
+            num=sum;
+        }
+        return num;
+
     }
 }
