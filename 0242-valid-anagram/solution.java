19621def42aab3e4d72955bbca18fefa4ba50580
BHUVESH ROHANKAR <bhuveshrohankar28@gmail.com>
2026-02-28 19:04:22 +0000

    Add solution Runtime - 6 ms (43.74%), Memory - 44.4 MB (74.79%)

--- 0242-valid-anagram/solution.java
+++ 0242-valid-anagram/solution.java
@@ -6,11 +6,17 @@ class Solution {
         if (s.length()!=t.length()) {
             return false;
         }
-       char[] sChars=s.toCharArray();
-       char[] tChars=t.toCharArray();
-        Arrays.sort(sChars);
-        Arrays.sort(tChars);
-        return Arrays.equals(sChars,tChars);
+        int [] count = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            count[s.charAt(i)-'a']++;
+            count[t.charAt(i)-'a']--;
+
+            
+        }for (int val : count) {
+            if (val!=0) {
+                return false;
+            }
+        }return true;
 
     }
 }
