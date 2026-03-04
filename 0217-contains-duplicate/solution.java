7a9d16066f92d4ada0c1fb72fd378ae66e075b5e
BHUVESH ROHANKAR <bhuveshrohankar28@gmail.com>
2026-03-04 13:15:12 +0000

    Sync LeetCode submission Runtime - 24 ms (25.78%), Memory - 76.9 MB (79.47%)

---
+++ 0217-contains-duplicate/solution.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        int n=nums.length;
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length-1;i++){
+            if(nums[i]==nums[i+1]) return true;
+        }return false;
+    }
+}
