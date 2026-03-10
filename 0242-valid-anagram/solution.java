15fa701585a38491b60d01fb778220df81b2d2df
BHUVESH ROHANKAR <bhuveshrohankar28@gmail.com>
2026-03-10 21:14:24 +0000

    Add solution Runtime - 15 ms (23.99%), Memory - 46.5 MB (25.01%)

--- 0242-valid-anagram/solution.java
+++ 0242-valid-anagram/solution.java
@@ -1,22 +1,21 @@
-
-import java.util.Arrays;
-
 class Solution {
     public boolean isAnagram(String s, String t) {
         if (s.length()!=t.length()) {
             return false;
-        }
-        int [] count = new int[26];
-        for (int i = 0; i < s.length(); i++) {
-            count[s.charAt(i)-'a']++;
-            count[t.charAt(i)-'a']--;
-
-            
-        }for (int val : count) {
-            if (val!=0) {
-                return false;
-            }
-        }return true;
-
+        } 
+    HashMap<Character,Integer> aMap=new HashMap<>();
+    for (char ele : s.toCharArray()) {
+        aMap.put(ele, aMap.getOrDefault(ele, 0) + 1);
+    }
+    for (char ele : t.toCharArray()) {
+        if (!aMap.containsKey(ele)) {
+            return false;
+        }if(aMap.get(ele)==0) return false;
+        aMap.put(ele, aMap.get(ele) - 1);
+        // if (aMap.get(ele) < 0) {
+        //     return false;
+        // }
+    }
+    return true;
     }
 }
