d72cd1dc1f03b4dfbacfdbfbc39d71bec9d76819
BHUVESH ROHANKAR <bhuveshrohankar28@gmail.com>
2026-03-14 08:57:35 +0000

    Add solution Runtime - 6 ms (50.35%), Memory - 55.6 MB (46.28%)

--- 0169-majority-element/solution.java
+++ 0169-majority-element/solution.java
@@ -1,11 +1,8 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        int count=1,currentNum=nums[0];
-        for(int i=1;i<nums.length;i++){
-            if(nums[i]==currentNum) count++;
-            else if(count==0){
-                currentNum=nums[i];
-            }else count--;
-        }return currentNum;
+        int n=nums.length;
+        Arrays.sort(nums);
+        return nums[n/2];
+        
     }
 }
