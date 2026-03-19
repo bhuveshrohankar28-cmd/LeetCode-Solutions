07efaa3a0df051da06e3421cc9b4b41bdb139711
BHUVESH ROHANKAR <bhuveshrohankar28@gmail.com>
2026-03-19 08:28:00 +0000

    Add solution Runtime - 3 ms (57.24%), Memory - 47.4 MB (10.95%)

--- 0001-two-sum/solution.java
+++ 0001-two-sum/solution.java
@@ -1,16 +1,12 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-      
-
-
-
-    for (int i = 0; i < nums.length; i++) {
-        for (int j = i+1; j < nums.length; j++) {
-            if (nums[i]+nums[j]==target) {
-                return new int[]{i,j};
-            }
+        HashMap<Integer,Integer> set=new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            int complement=target-nums[i];
+            if(set.containsKey(complement)) return new int[]{set.get(complement),i};
+            set.put(nums[i],i);
         }
-    }return new int[0];
+          return new int[]{};
+
     }
-    
 }
