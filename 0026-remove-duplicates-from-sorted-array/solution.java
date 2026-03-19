dcf2fe1df88ac290e940a03ccea3596610bf8407
BHUVESH ROHANKAR <bhuveshrohankar28@gmail.com>
2026-03-19 09:03:30 +0000

    Add solution Runtime - 1 ms (78.53%), Memory - 46.9 MB (9.12%)

--- 0026-remove-duplicates-from-sorted-array/solution.java
+++ 0026-remove-duplicates-from-sorted-array/solution.java
@@ -1,13 +1,12 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        int n = nums.length;
-        int i = 0;
-        for (int j = 1; j < n; j++) {
-            if (nums[i] != nums[j]) {
-                i++;
+        int i = 1;
+        for (int j = 1; j < nums.length; j++) {
+            if (nums[j - 1] != nums[j]) {
                 nums[i] = nums[j];
+                i++;
             }
         }
-        return i + 1;
+        return i;
     }
 }
