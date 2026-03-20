cd580401c5dff84facca6bfeaa87a225091bc2e9
BHUVESH ROHANKAR <bhuveshrohankar28@gmail.com>
2026-03-20 17:34:36 +0000

    Sync LeetCode submission Runtime - 1 ms (100.00%), Memory - 43.8 MB (66.04%)

---
+++ 3408-count-the-number-of-special-characters-i/solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int numberOfSpecialChars(String word) {
+        int ans = 0;
+        int[] counts = new int[128];
+        for (char ele : word.toCharArray()) {
+            counts[ele]++;
+
+        }
+        for (char i = 'A'; i <= 'Z'; i++) {
+            if (counts[i] > 0 && counts[i + 32] > 0) {
+                ans++;
+            }
+        }
+        return ans;
+    }
+}
