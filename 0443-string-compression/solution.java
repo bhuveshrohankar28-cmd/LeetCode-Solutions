4c494b0194b2b187ea42c83ac6d944af47edc6ec
BHUVESH ROHANKAR <bhuveshrohankar28@gmail.com>
2026-03-20 20:40:13 +0000

    Sync LeetCode submission Runtime - 1 ms (99.89%), Memory - 44.7 MB (99.55%)

---
+++ 0443-string-compression/solution.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int compress(char[] chars) {
+        int write = 0; // Where we write the result
+        int i = 0;     // Start of the current group
+
+        while (i < chars.length) {
+            int j = i;
+            // Find the end of the group of identical characters
+            while (j < chars.length && chars[j] == chars[i]) {
+                j++;
+            }
+
+            // 1. Write the character
+            chars[write++] = chars[i];
+
+            // 2. If the group is larger than 1, write the count
+            int count = j - i;
+            if (count > 1) {
+                // Convert count to string to handle multi-digit numbers (like 12)
+                for (char c : Integer.toString(count).toCharArray()) {
+                    chars[write++] = c;
+                }
+            }
+
+            // 3. Move i to the start of the next group
+            i = j;
+        }
+        return write; // The new length of the compressed array
+    }
+}
