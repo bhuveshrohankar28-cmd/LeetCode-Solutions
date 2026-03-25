ac695cb2840e91271a9b46059f070babedb14676
BHUVESH ROHANKAR <bhuveshrohankar28@gmail.com>
2026-03-25 00:22:59 +0000

    Add solution Runtime - 0 ms (100.00%), Memory - 42.8 MB (91.88%)

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/solution.java
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/solution.java
@@ -1,11 +1,6 @@
 class Solution {
-    public int strStr(String haystack, String needle) {//O(N)
-        for(int i = 0, j = needle.length(); j<=haystack.length(); i++,j++){
-            if(haystack.substring(i,j).equals(needle)){
-                return i;
-            }
-        }
-        return -1;
+    public int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle);
     }
 }
 
