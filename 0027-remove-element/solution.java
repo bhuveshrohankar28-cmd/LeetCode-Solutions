bfa4c3bdf3141d24587f89a5b080a70a35e63a4e
BHUVESH ROHANKAR <bhuveshrohankar28@gmail.com>
2026-04-02 16:51:57 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 43.4 MB (67.22%)

--- 0027-remove-element/solution.java
+++ 0027-remove-element/solution.java
@@ -1,13 +1,8 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
         int j=0;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]!=val){
-                nums[j++]=nums[i];
-
-            }
+        for(int ele:nums){
+            if(ele!=val) nums[j++]=ele;
         }return j;
-        
-        
     }
 }
