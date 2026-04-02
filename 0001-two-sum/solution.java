1182d285b5710bd6bc1ed3609d7be4a3fdf6274d
BHUVESH ROHANKAR <bhuveshrohankar28@gmail.com>
2026-04-02 17:09:46 +0000

    Add solution Runtime - 2 ms (99.34%), Memory - 46.9 MB (83.78%)

--- 0001-two-sum/solution.java
+++ 0001-two-sum/solution.java
@@ -3,10 +3,8 @@ class Solution {
         HashMap<Integer,Integer> map=new HashMap<>();
         for(int i=0;i<nums.length;i++){
             int complement=target-nums[i];
-            if(map.containsKey(complement)){
-                return new int[] {i,map.get(complement)};
-            }map.put(nums[i],i);
-
-        }return new int[]  {0,0};
+            if(map.containsKey(complement)) return new int[]{map.get(complement),i};
+            map.put(nums[i],i);
+        }return new int[]{};
     }
 }
