30ce51e3ec747fbb10cfdd38c1e0f0b48f797388
BHUVESH ROHANKAR <bhuveshrohankar28@gmail.com>
2026-04-02 16:32:55 +0000

    Add solution Runtime - 0 ms (100.00%), Memory - 80.2 MB (77.05%)

--- 0882-peak-index-in-a-mountain-array/solution.java
+++ 0882-peak-index-in-a-mountain-array/solution.java
@@ -3,10 +3,11 @@ class Solution {
         int low=1,high=arr.length-2;
         int mid=0;
         while(low<=high){
-            mid=low+(high-low)/2;
-            if(arr[mid]>arr[mid-1]&&arr[mid]>arr[mid+1])return mid;
-            if(arr[mid]<arr[mid+1])low=mid+1; 
+            mid=low +(high-low)/2;
+            if(arr[mid]>arr[mid-1]&&arr[mid]>arr[mid+1]) return mid;
+            if(arr[mid]>arr[mid-1]&&arr[mid]<arr[mid+1])low =mid+1 ;
             else high=mid-1;
+            
         }return -1;
     }
 }
