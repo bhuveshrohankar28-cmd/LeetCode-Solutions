a07dc6763d8a16335ec47be690327c7fd7abe848
BHUVESH ROHANKAR <bhuveshrohankar28@gmail.com>
2026-04-03 11:47:11 +0000

    Add solution Runtime - 1 ms (99.98%), Memory - 42.6 MB (63.64%)

--- 0007-reverse-integer/solution.java
+++ 0007-reverse-integer/solution.java
@@ -1,19 +1,12 @@
 class Solution {
     public int reverse(int x) {
-       
-        
-        long  temp=x;
-      long sum=0;
-      long rem=0;
-        while (temp!=0) {
-             rem=temp%10;
-            sum=(sum*10)+rem;
-            temp/=10;}
-        if(sum>Integer.MAX_VALUE||sum<Integer.MIN_VALUE){
-            return 0;
-        }
-        return (int)sum;
+       int rev=0;
+       while(x!=0){
+        int last=x%10;
+         x/=10;
+         if(rev>Integer.MAX_VALUE/10||rev==Integer.MAX_VALUE/10&&last>7) return 0;
+         if(rev<Integer.MIN_VALUE/10||rev==Integer.MIN_VALUE/10&&last<-8) return 0;
+         rev=rev*10+last;
+       }return rev;
     }
 }
-    
-
