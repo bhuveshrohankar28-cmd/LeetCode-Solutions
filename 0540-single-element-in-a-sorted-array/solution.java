620bfa17dc7f7f404feefa481a970f5fb44dd588
BHUVESH ROHANKAR <bhuveshrohankar28@gmail.com>
2026-04-04 17:16:04 +0000

    Add solution Runtime - 0 ms (100.00%), Memory - 52.7 MB (89.89%)

--- 0540-single-element-in-a-sorted-array/solution.java
+++ 0540-single-element-in-a-sorted-array/solution.java
@@ -1,18 +1,19 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-      int n=nums.length,mid=0; 
-      int low=0,high=n-1;
-       if(n==1)  return nums[0];
-       if(nums[0]!=nums[1]) return nums[0];
-       if(nums[n-1]!=nums[n-2]) return nums[n-1];
+      int n=nums.length,mid=0;
+      int low = 0,high = n-1;
+      if(n==1) return nums[0];
+      if(nums[0]!=nums[1]) return nums[0];
+      if(nums[n-1] != nums[n-2]) return nums[n-1];
       while(low<=high){
-        mid = low+(high-low)/2;
-        if(nums[mid]!=nums[mid-1]&&nums[mid]!=nums[mid+1]) return nums[mid];
+        mid=low +( high-low)/2;
+        if(nums[mid]!=nums[mid-1]&&nums[mid]!=nums[mid+1])
+            return nums[mid];
         int f=mid,s=mid;
-        if(nums[mid]==nums[mid-1]) f = mid-1;
+        if(nums[mid]==nums[mid-1]) f=mid-1;
         else s=mid+1;
-        if(f%2==0) low=s+1;
-        else high =f-1;
+        if(f%2==0) low = s+1;
+        else high = f-1;
       }return 0;
     }
 }
