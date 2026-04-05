4cc1fe5b4428d83b8e4b0450d0939a6ed5352740
BHUVESH ROHANKAR <bhuveshrohankar28@gmail.com>
2026-04-05 09:55:30 +0000

    Sync LeetCode submission Runtime - 1 ms (97.78%), Memory - 42.9 MB (12.95%)

--- 0069-sqrtx/solution.java
+++ 0069-sqrtx/solution.java
@@ -1,10 +1,11 @@
 class Solution {
     public int mySqrt(int x) {
-        int low=0,high=x;
-        while(low<=high){
-            int mid=low+(high-low)/2;
-           if((long)mid*mid<=x) low=mid+1;
-           else high = mid -1;
-        }return (int)high;
+       if(x<2) return x;
+       long r=x;
+       while(r*r>x){
+        r=(r+x/r)/2;
+       }return (int) r;
+        
+       
     }
 }
