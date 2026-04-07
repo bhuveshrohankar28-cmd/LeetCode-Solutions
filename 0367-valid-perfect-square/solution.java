d1f039bac72a5a9d5854850ea9cc30f8cc2bc4f3
BHUVESH ROHANKAR <bhuveshrohankar28@gmail.com>
2026-04-07 23:49:05 +0000

    Add solution Runtime - 0 ms (100.00%), Memory - 42.1 MB (37.72%)

--- 0367-valid-perfect-square/solution.java
+++ 0367-valid-perfect-square/solution.java
@@ -4,8 +4,8 @@ class Solution {
         while(low<=high){
             int mid=low+(high-low)/2;
             if(mid*mid==num) return true;
-            if(mid>num/mid) high=mid-1;
-            else low=mid+1;
+            if(mid<num/mid) low=mid+1;
+            else high=mid-1;
         }return false;
     }
 }
