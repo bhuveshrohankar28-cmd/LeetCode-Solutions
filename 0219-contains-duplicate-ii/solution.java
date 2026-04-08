9bdbf459955a80c27c6bc7a5c19f294e5c4a0f19
BHUVESH ROHANKAR <bhuveshrohankar28@gmail.com>
2026-04-08 23:56:10 +0000

    Sync LeetCode submission Runtime - 20 ms (94.79%), Memory - 80.7 MB (53.48%)

--- 0219-contains-duplicate-ii/solution.java
+++ 0219-contains-duplicate-ii/solution.java
@@ -1,11 +1,11 @@
 class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
-        HashSet map=new HashSet<>();
+        HashSet<Integer> set=new HashSet<>();
         int j=0;
         for(int i=0;i<nums.length;i++){
-           if( !map.add(nums[i])) return true;
-            if(map.size()>k){
-                map.remove(nums[j++]);
+            if(!set.add(nums[i])) return true;
+            if(set.size()>k){
+                set.remove(nums[j++]);
             }
         }return false;
     }
