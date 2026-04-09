8b845f00fa6971b5cff454ebb93db5338ccffa7a
BHUVESH ROHANKAR <bhuveshrohankar28@gmail.com>
2026-04-09 14:28:21 +0000

    Add solution Runtime - 0 ms (100.00%), Memory - 46.4 MB (50.29%)

--- 0745-find-smallest-letter-greater-than-target/solution.java
+++ 0745-find-smallest-letter-greater-than-target/solution.java
@@ -1,11 +1,15 @@
 class Solution {
     public char nextGreatestLetter(char[] letters, char target) {
         int low=0,high=letters.length-1;
+        int found=-1;
         while(low<=high){
             int mid=low+(high-low)/2;
-            if(letters[mid]<=target) low = mid + 1;
-            else high=mid-1;
+            if(letters[mid]>target){
+                found=mid;
+                high=mid-1;
+            }else if(letters[mid]<=target) low=mid+1;
             
-        }return letters[low%letters.length];
+        }if(found==-1) return letters[0];
+        return letters[low];
     }
 }
