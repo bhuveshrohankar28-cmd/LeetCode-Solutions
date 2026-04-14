989a020d15e097dccedf18178c9fd0cf3ac3f58c
BHUVESH ROHANKAR <bhuveshrohankar28@gmail.com>
2026-04-14 09:51:53 +0000

    Add solution Runtime - 0 ms (100.00%), Memory - 44 MB (73.40%)

--- 0162-find-peak-element/solution.java
+++ 0162-find-peak-element/solution.java
@@ -1,11 +1,13 @@
 class Solution {
     public int findPeakElement(int[] nums) {
-        int n=nums.length;
-        int low=0,high=n-1;
-        while(low<high){
-            int mid=low+(high-low)/2;
-            if(nums[mid]<nums[mid+1]) low=mid+1;
-            else high=mid;
-        }return low;
+        int low = 0, high = nums.length - 1;
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] < nums[mid + 1])
+                low = mid+1;
+            else
+                high = mid ;
+        }
+        return low;
     }
 }
