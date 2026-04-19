40acc4b9a7bfe3bfecab2b56ec3adaebf77e576d
BHUVESH ROHANKAR <bhuveshrohankar28@gmail.com>
2026-04-19 20:51:53 +0000

    Add solution Runtime - 0 ms (100.00%), Memory - 43 MB (96.40%)

--- 1137-height-checker/solution.java
+++ 1137-height-checker/solution.java
@@ -1,25 +1,14 @@
 class Solution {
     public int heightChecker(int[] arr) {
-        int n = arr.length;
-        int[] expected = arr.clone();
-        int count = 0;
-        for (int i = 0; i < n - 1; i++) {
-            int minIdx = i;
-            for (int j = i + 1; j < n; j++) {
-                if (expected[j] < expected[minIdx]) {
-                    minIdx = j;
-                }
-            }
-
-            int temp = expected[minIdx];
-            expected[minIdx] = expected[i];
-            expected[i] = temp;
-        }
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] != expected[i])
-                count++;
-        }
-        return count;
+        int[] freqArr =new int[101];
+        for(int ele:arr) freqArr[ele]++;
+        int currEle=0;
+        int result=0;
+        for(int ele:arr){
+            while(freqArr[currEle]==0) currEle++;
+            if(currEle!=ele) result++;
+            freqArr[currEle]--;
+        }return result;
     }
 
 }
