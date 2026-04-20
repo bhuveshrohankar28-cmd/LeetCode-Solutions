855dbfb82b9224b34a91eaf3d8606fe13010f863
BHUVESH ROHANKAR <bhuveshrohankar28@gmail.com>
2026-04-20 19:07:17 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 43.6 MB (27.74%)

--- 0075-sort-colors/solution.java
+++ 0075-sort-colors/solution.java
@@ -3,16 +3,16 @@ class Solution {
         int n = nums.length;
         int left = 0, moving = 0, right = n - 1;
         while (moving <= right) {
-            if (nums[moving] == 0) {
-                int temp = nums[left];
-                nums[left++] = nums[moving];
-                nums[moving++] = temp;
+            if (nums[moving] == 2) {
+                int temp = nums[right];
+                nums[right--] = nums[moving];
+                nums[moving] = temp;
             } else if (nums[moving] == 1) {
                 moving++;
             } else {
-                int temp = nums[moving];
-                nums[moving] = nums[right];
-                nums[right--] = temp;
+                int temp = nums[left];
+                nums[left++] = nums[moving];
+                nums[moving++] = temp;
             }
         }
     }
