9d66d951f446e5f7dabc445cc3172f25ba8c8e51
BHUVESH ROHANKAR <bhuveshrohankar28@gmail.com>
2026-04-24 21:37:09 +0000

    Add solution Runtime - 0 ms (100.00%), Memory - 46.9 MB (27.64%)

--- 0898-transpose-matrix/solution.java
+++ 0898-transpose-matrix/solution.java
@@ -2,10 +2,11 @@ class Solution {
     public int[][] transpose(int[][] matrix) {
         int rows=matrix.length,cols=matrix[0].length;
         int[][] newMat=new int[cols][rows];
-        for(int i=0;i<newMat.length;i++){
-            for(int j=0;j<newMat[0].length;j++){
+        for(int i=0;i<cols;i++){
+            for(int j=0;j<rows;j++){
                 newMat[i][j]=matrix[j][i];
             }
         }return newMat;
+
     }
 }
