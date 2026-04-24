0c993ca703a9d0d7b3bc76728057094377f270f7
BHUVESH ROHANKAR <bhuveshrohankar28@gmail.com>
2026-04-24 10:55:23 +0000

    Add solution Runtime - 25 ms (33.65%), Memory - 76.2 MB (85.05%)

--- 0219-contains-duplicate-ii/solution.java
+++ 0219-contains-duplicate-ii/solution.java
@@ -1,9 +1,10 @@
 class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
-        HashSet<Integer> set=new HashSet<>();
+        HashSet<Integer> set =new HashSet<>();
         int j=0;
         for(int i=0;i<nums.length;i++){
-            if(!set.add(nums[i])) return true;
+            if(set.contains(nums[i])) return true;
+            set.add(nums[i]);
             if(set.size()>k){
                 set.remove(nums[j++]);
             }
