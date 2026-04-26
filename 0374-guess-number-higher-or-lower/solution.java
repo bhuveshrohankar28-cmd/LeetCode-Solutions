d4642209596f7087763d846dac1e255a8954b71a
BHUVESH ROHANKAR <bhuveshrohankar28@gmail.com>
2026-04-26 22:52:58 +0000

    Add solution Runtime - 0 ms (100.00%), Memory - 42.1 MB (52.94%)

--- 0374-guess-number-higher-or-lower/solution.java
+++ 0374-guess-number-higher-or-lower/solution.java
@@ -9,13 +9,14 @@
 
 public class Solution extends GuessGame {
     public int guessNumber(int n) {
-       int low=1,high=n;
-       while(low<=high){
-        int mid=low+(high-low)/2;
-        int num= guess(mid);
-        if(num==0) return mid;
-        if(num==-1) high=mid-1;
-        else low=mid+1;
-       } return 0;
+        int low=0,high=n;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            int guess=guess(mid);
+            if(guess==0) return mid;
+            if(guess==-1) high=mid-1;
+            else low =mid+1;
+
+        }return 0;
     }
 }
