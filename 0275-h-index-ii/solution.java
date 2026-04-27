5c7017da5bd2ea23d5d2ac6cd4ca65706b4ac6a1
BHUVESH ROHANKAR <bhuveshrohankar28@gmail.com>
2026-04-27 21:56:22 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 50.2 MB (66.93%)

--- 0275-h-index-ii/solution.java
+++ 0275-h-index-ii/solution.java
@@ -1,16 +1,14 @@
 class Solution {
     public int hIndex(int[] citations) {
-        int n = citations.length;
-        int low = 0, high = n - 1;
-        int h =0;
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            if (n - mid <= citations[mid]) {
-                high = mid-1;
-                h = n - mid;
-            } else
-                low = mid+1 ;
-        }
-        return h;
+        int n=citations.length;
+        int low = 0,high=n-1;
+        int h=0;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(n-mid<=citations[mid]){
+                h=n-mid;
+                high=mid-1;
+            }else low=mid+1;
+        }return h;
     }
 }
