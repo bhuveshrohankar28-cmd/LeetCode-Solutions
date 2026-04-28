23b06457851af38f7fb2876fbef508fe215c7038
BHUVESH ROHANKAR <bhuveshrohankar28@gmail.com>
2026-04-28 22:08:37 +0000

    Add solution Runtime - 0 ms (100.00%), Memory - 50 MB (94.18%)

--- 0275-h-index-ii/solution.java
+++ 0275-h-index-ii/solution.java
@@ -1,14 +1,16 @@
 class Solution {
     public int hIndex(int[] citations) {
-        int n=citations.length;
-        int low = 0,high=n-1;
-        int h=0;
-        while(low<=high){
-            int mid=low+(high-low)/2;
-            if(n-mid<=citations[mid]){
-                h=n-mid;
-                high=mid-1;
-            }else low=mid+1;
-        }return h;
+        int n = citations.length;
+        int low = 0, high = n - 1;
+        int h = 0;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (n - mid <= citations[mid]) {
+                h = n - mid;
+                high = mid - 1;
+            } else
+                low = mid + 1;
+        }
+        return h;
     }
 }
