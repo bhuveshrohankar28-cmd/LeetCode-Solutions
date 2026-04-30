1cfe23ff585769741696bff38ce873e05c05f685
BHUVESH ROHANKAR <bhuveshrohankar28@gmail.com>
2026-04-30 17:55:11 +0000

    Add solution Runtime - 0 ms (100.00%), Memory - 41.9 MB (69.69%)

--- 1013-fibonacci-number/solution.java
+++ 1013-fibonacci-number/solution.java
@@ -1,12 +1,12 @@
 class Solution {
     public int fib(int n) {
-        int a = 0, b = 1;
-        int next = n;
-        for (int i = 2; i <= n; i++) {
-            next = a + b;
-            a = b;
-            b = next;
-        }
-        return next;
+        if(n<=1)return n;
+        int a=0,b=1;
+        int next=0;
+        for(int i=2;i<=n;i++){
+            next=a+b;
+            a=b;
+            b=next;
+        }return next;
     }
 }
