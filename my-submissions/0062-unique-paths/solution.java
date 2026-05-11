7ea3baa6ca49c6537e203b48db04d965120db4cf
BHUVESH ROHANKAR <bhuveshrohankar28@gmail.com>
2026-05-11 23:52:32 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 42 MB (64.54%)

---
+++ my-submissions/0062-unique-paths/solution.java
@@ -0,0 +1,11 @@
+public class Solution {
+    public int uniquePaths(int m, int n) {
+        long res = 1;
+        int j = 1;
+        for(int i=Math.max(m,n); i <= m+n-2; i++,j++){// Instead of taking factorial, keep on multiply & divide
+            res *= i; 
+            res/=j;
+        }   
+        return (int)res;
+    }
+}
