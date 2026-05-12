15e00eb9806031553ff6df70732e2fcda6817d2a
BHUVESH ROHANKAR <bhuveshrohankar28@gmail.com>
2026-05-12 22:10:09 +0000

    Add solution Runtime - 3 ms (89.12%), Memory - 48.3 MB (26.15%)

--- 0240-search-a-2d-matrix-ii/solution.java
+++ 0240-search-a-2d-matrix-ii/solution.java
@@ -1,7 +1,7 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         int rows=0,cols=matrix[0].length-1;
-        while(rows<matrix.length&&cols>=0){
+        while(rows<matrix.length && cols>=0){//O(n)
             if(target==matrix[rows][cols]) return true;
             if(target<matrix[rows][cols]) cols--;
             else rows++;
