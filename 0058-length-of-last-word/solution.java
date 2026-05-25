66d65e0e696a49e22eabf3822e04412fa6a14a69
BHUVESH ROHANKAR <bhuveshrohankar28@gmail.com>
2026-05-25 15:09:00 +0000

    Add solution Runtime - 0 ms (100.00%), Memory - 43.1 MB (33.90%)

--- 0058-length-of-last-word/solution.java
+++ 0058-length-of-last-word/solution.java
@@ -1,10 +1,20 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        int count=0;
-        for(int i=s.length()-1;i>=0;i--){
-            if(s.charAt(i)==' '&&count!=0) return count;
-            else if(s.charAt(i)!=' ') count++;
 
-        }return count;
+        int length = 0;
+        int i = s.length() - 1;
+
+        // Skip trailing spaces
+        while (i >= 0 && s.charAt(i) == ' ') {
+            i--;
+        }
+
+        // Count last word length
+        while (i >= 0 && s.charAt(i) != ' ') {
+            length++;
+            i--;
+        }
+
+        return length;
     }
 }
