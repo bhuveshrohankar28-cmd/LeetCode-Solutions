c04e74dea37c4bd21b0f4962495715a3645c22e3
BHUVESH ROHANKAR <bhuveshrohankar28@gmail.com>
2026-05-29 15:49:49 +0000

    Add solution Runtime - 2 ms (99.65%), Memory - 43 MB (69.72%)

--- 0038-count-and-say/solution.java
+++ 0038-count-and-say/solution.java
@@ -1,11 +1,11 @@
 class Solution {
     public String countAndSay(int n) {
-        if(n==1) return "1";
+        if(n==1) return "1";//
         String s=countAndSay(n-1);
         StringBuilder ans=new StringBuilder();
         int i=0;
-        while(i<s.length()){
-            int j=i;
+        while(i < s.length()){
+            int j = i;
             while(j<s.length()&&s.charAt(i)==s.charAt(j)) j++;
             int len=j-i;
             ans.append(len);
