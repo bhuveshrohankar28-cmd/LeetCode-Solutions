b71530d11ddedf88133e8ebee44f46cf29941c7e
BHUVESH ROHANKAR <bhuveshrohankar28@gmail.com>
2026-05-30 14:11:28 +0000

    Add solution Runtime - 0 ms (100.00%), Memory - 43.8 MB (46.54%)

--- 0033-search-in-rotated-sorted-array/solution.java
+++ 0033-search-in-rotated-sorted-array/solution.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int search(int[] nums, int target) {
+    public int search(int[] nums, int target) {//O(N)
         int low = 0, high = nums.length - 1;
         while (low <= high) {
             int mid = low + (high - low) / 2;
