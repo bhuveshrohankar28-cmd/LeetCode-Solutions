6ad676409e22037b0d19d08ff49b6c69d0119529
BHUVESH ROHANKAR <bhuveshrohankar28@gmail.com>
2026-05-31 10:41:59 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 42.5 MB (68.20%)

---
+++ my-submissions/0119-pascals-triangle-ii/solution.java
@@ -0,0 +1,10 @@
+class Solution {
+    public List<Integer> getRow(int rowIndex) {
+        List<Integer> ans =new ArrayList<>();//O(N)
+        long current=1;
+        for(int i=0;i<=rowIndex;i++){
+            ans.add((int)current);
+            current=current*(rowIndex-i)/(i+1);
+        }return ans;
+    }
+}
