d8d18dc3d8f2c089691c897e6c8c1dd2d61f52d2
BHUVESH ROHANKAR <bhuveshrohankar28@gmail.com>
2026-06-02 15:46:52 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 48.4 MB (10.61%)

---
+++ my-submissions/0050-powx-n/solution.java
@@ -0,0 +1,14 @@
+class Solution {//again
+    public double myPow(double x, long n) {
+        if(n<0){ 
+            x=1/x;
+            n=-n;
+        }
+         if(n==0) return 1;
+         if(n==1) return x;
+         double halfPower= myPow(x,n/2);
+         if(n%2==0) return halfPower*halfPower;
+         else return x*halfPower*halfPower; 
+
+    }
+}
