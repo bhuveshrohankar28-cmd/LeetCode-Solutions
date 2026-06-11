a081d7f0f844f58f81941559294d0077fb8cc470
BHUVESH ROHANKAR <bhuveshrohankar28@gmail.com>
2026-06-11 18:18:32 +0000

    Sync LeetCode submission Runtime - 5 ms (3.65%), Memory - 43.9 MB (26.60%)

---
+++ my-submissions/0088-merge-sorted-array/solution.java
@@ -0,0 +1,9 @@
+class Solution {//again
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int j=0;
+        for(int i=0;i<nums1.length;i++){
+            if(nums1[i]==0&&j<nums2.length) nums1[i]=nums2[j++];
+
+        }Arrays.sort(nums1);
+    }
+}
