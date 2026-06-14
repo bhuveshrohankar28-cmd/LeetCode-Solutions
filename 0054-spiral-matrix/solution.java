d5423addc581c0e0d07a35670ca41f271b4917e8
BHUVESH ROHANKAR <bhuveshrohankar28@gmail.com>
2026-06-14 17:56:19 +0000

    Add solution Runtime - 0 ms (100.00%), Memory - 42.9 MB (79.44%)

--- 0054-spiral-matrix/solution.java
+++ 0054-spiral-matrix/solution.java
@@ -1,33 +1,36 @@
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
-        List<Integer> list = new ArrayList<>();
-        int firstRow = 0, firstCol = 0;
-        int lastRow = matrix.length-1, lastCol = matrix[0].length-1;
-        while (lastRow >= firstRow && lastCol >= firstCol) {
-            for (int i = firstCol; i <= lastCol; i++) {
-                list.add(matrix[firstRow][i]);
-            }
-            firstRow++;
-            if (firstRow > lastRow || firstCol > lastCol)
-                break;
-            for (int i = firstRow; i <= lastRow; i++) {
-                list.add(matrix[i][lastCol]);
-            }
-            lastCol--;
-            if (firstRow > lastRow || firstCol > lastCol)
-                break;
-            for (int i = lastCol; i >= firstCol; i--) {
-                list.add(matrix[lastRow][i]);
-            }
-            lastRow--;
-            if (firstRow > lastRow || firstCol > lastCol)
-                break;
-            for (int i = lastRow; i >= firstRow; i--) {
-                list.add(matrix[i][firstCol]);
-            }
-            firstCol++;
 
+        ArrayList<Integer> ans = new ArrayList<>();
+        int m = matrix.length, n = matrix[0].length;
+        int firstRow = 0, lastRow = m - 1;
+        int firstCol = 0, lastCol = n - 1;
+
+        while (lastRow >= firstRow &&
+               lastCol >= firstCol) {
+            // 1. Left to Right
+            for (int i = firstCol; i <= lastCol; i++)
+                ans.add(matrix[firstRow][i]);
+            firstRow++;
+            if (firstRow > lastRow ||firstCol > lastCol) break;
+               
+            // 2. Top to Bottom
+            for (int i = firstRow; i <= lastRow; i++)
+                ans.add(matrix[i][lastCol]);
+            lastCol--;
+            if (firstRow > lastRow ||firstCol > lastCol) break;
+                
+            // 3. Right to Left
+            for (int i = lastCol; i >= firstCol; i--)
+                ans.add(matrix[lastRow][i]);
+            lastRow--;
+            if (firstRow > lastRow ||firstCol > lastCol) break;
+                
+            // 4. Bottom to Top
+            for (int i = lastRow; i >= firstRow; i--)
+                ans.add(matrix[i][firstCol]);
+            firstCol++;
         }
-        return list;
+        return ans;
     }
 }
