c325ae6c522376e32323472b032821a561e9c932
BHUVESH ROHANKAR <bhuveshrohankar28@gmail.com>
2026-06-15 14:08:20 +0000

    Sync LeetCode submission Runtime - 1 ms (99.91%), Memory - 42.9 MB (92.43%)

---
+++ my-submissions/0389-find-the-difference/solution.java
@@ -0,0 +1,8 @@
+class Solution {//
+    public char findTheDifference(String s, String t) {
+        int ans=0;
+        for(char ele:s.toCharArray()) ans=ans^ele;
+        for(char ele:t.toCharArray()) ans=ans^ele;
+        return (char)ans;
+    }
+}
