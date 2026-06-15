d78ebb576bd1b43a79683f023cbfd50415970b5f
BHUVESH ROHANKAR <bhuveshrohankar28@gmail.com>
2026-06-15 13:50:24 +0000

    Sync LeetCode submission Runtime - 1 ms (99.88%), Memory - 55.6 MB (59.58%)

---
+++ my-submissions/0169-majority-element/solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int count=1,currentNum=nums[0];
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]==currentNum) count++;
+            else if(count==0){
+                currentNum=nums[i];
+            }else count--;
+        }return currentNum;
+    }
+}
