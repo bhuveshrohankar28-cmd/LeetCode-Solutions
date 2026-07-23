d7c4c7510d0f665ed05e8006c53837ed7dc380fb
BHUVESH ROHANKAR <bhuveshrohankar28@gmail.com>
2026-07-23 20:18:06 +0000

    Add solution Runtime - 0 ms (100.00%), Memory - 43.6 MB (43.70%)

--- 0075-sort-colors/solution.java
+++ 0075-sort-colors/solution.java
@@ -1,19 +1,21 @@
-class Solution {//again
+class Solution {
     public void sortColors(int[] nums) {
-        int n = nums.length;
-        int left = 0, moving = 0, right = n - 1;
-        while (moving <= right) {
-            if (nums[moving] == 2) {
-                int temp = nums[right];
-                nums[right--] = nums[moving];
-                nums[moving] = temp;
-            } else if (nums[moving] == 1) {
-                moving++;
-            } else {
-                int temp = nums[left];
-                nums[left++] = nums[moving];
-                nums[moving++] = temp;
+        int low = 0;
+        int high = nums.length-1;
+        for(int i=0;i<=high;i++){
+            if(nums[i]==0){
+                swap(i,low,nums);
+                low++;
+            }
+            if(nums[i]==2){
+                swap(i,high,nums);
+                high--;
+                i--;
             }
         }
-    }
+    }public static void swap(int i,int j,int[]nums){
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+        }
 }
