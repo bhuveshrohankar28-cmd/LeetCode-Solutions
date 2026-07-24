304fc7e22b099e8fdb06b0cb838ed12f9806a9b7
BHUVESH ROHANKAR <bhuveshrohankar28@gmail.com>
2026-07-24 17:57:59 +0000

    Add solution Runtime - 4 ms (41.79%), Memory - 175.6 MB (42.62%)

--- 2271-rearrange-array-elements-by-sign/solution.java
+++ 2271-rearrange-array-elements-by-sign/solution.java
@@ -1,16 +1,17 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
-        int n=nums.length;
-        int[] Temp=new int[n];
-        int posIndex=0,negIndex=1;
-        for(int ele:nums){
-            if(ele>0){
-                Temp[posIndex]=ele;
-                posIndex+=2;
-            }else {
-              Temp[negIndex]=ele;
-                negIndex+=2;}
+        int n = nums.length;
+        int[] arr = new int[n];
+        int evenIdx = 0, oddIdx = 1;
+        for (int ele : nums) {
+            if (ele > 0) {
+                arr[evenIdx] = ele;
+                evenIdx += 2;
+            } else {
+                arr[oddIdx] = ele;
+                oddIdx += 2;
+            }
         }
-        return Temp;
+            return arr;
     }
 }
