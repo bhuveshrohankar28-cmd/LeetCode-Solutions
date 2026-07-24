bca5747319d9d94b7fe4151d254b7b9cb75fed88
BHUVESH ROHANKAR <bhuveshrohankar28@gmail.com>
2026-07-24 06:53:58 +0000

    Add solution Runtime - 1 ms (99.94%), Memory - 94.6 MB (33.14%)

--- 0121-best-time-to-buy-and-sell-stock/solution.java
+++ 0121-best-time-to-buy-and-sell-stock/solution.java
@@ -1,10 +1,11 @@
-class Solution {//again
+class Solution {
     public int maxProfit(int[] prices) {
-        int buy=-1,sell=-1,Xbuy= Integer.MAX_VALUE,Xprofit=0;
-        for(int ele:prices){
-            Xbuy=Math.min(Xbuy,ele);
-            int profit=ele-Xbuy;
-            Xprofit=Math.max(Xprofit,profit);
-        }return Xprofit;
+        int profit = 0;
+        int minCost = prices[0];
+        for(int i=0;i<prices.length;i++){
+            if(prices[i]<minCost) minCost=prices[i];
+            profit=Math.max(profit,prices[i]-minCost);
+        }if(profit<=0) return 0;
+        else return profit;
     }
 }
