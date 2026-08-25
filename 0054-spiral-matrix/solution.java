15d0578c64c015abec5a1300fbd7e4a86fc056d9
BHUVESH ROHANKAR <bhuveshrohankar28@gmail.com>
2026-08-25 22:13:56 +0000

    Add solution Runtime - 0 ms (100.00%), Memory - 42.8 MB (80.19%)

--- 0054-spiral-matrix/solution.java
+++ 0054-spiral-matrix/solution.java
@@ -1,35 +1,33 @@
-class Solution {//again
+class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
-
-        ArrayList<Integer> ans = new ArrayList<>();
-        int m = matrix.length, n = matrix[0].length;
-        int firstRow = 0, lastRow = m - 1;
-        int firstCol = 0, lastCol = n - 1;
-
-        while (lastRow >= firstRow &&
-               lastCol >= firstCol) {
-            // 1. Left to Right
-            for (int i = firstCol; i <= lastCol; i++)
-                ans.add(matrix[firstRow][i]);
-            firstRow++;
-            if (firstRow > lastRow ||firstCol > lastCol) break;
-               
-            // 2. Top to Bottom
-            for (int i = firstRow; i <= lastRow; i++)
-                ans.add(matrix[i][lastCol]);
-            lastCol--;
-            if (firstRow > lastRow ||firstCol > lastCol) break;
-                
-            // 3. Right to Left
-            for (int i = lastCol; i >= firstCol; i--)
-                ans.add(matrix[lastRow][i]);
-            lastRow--;
-            if (firstRow > lastRow ||firstCol > lastCol) break;
-                
-            // 4. Bottom to Top
-            for (int i = lastRow; i >= firstRow; i--)
-                ans.add(matrix[i][firstCol]);
-            firstCol++;
+        List<Integer> ans = new ArrayList<Integer>();
+        int startRow = 0;
+        int endRow = matrix.length - 1;
+        int startCol = 0;
+        int endCol = matrix[0].length - 1;
+        while (startRow <= endRow && startCol <= endCol) {
+            for (int i = startCol; i <= endCol; i++) {
+                ans.add(matrix[startRow][i]);
+            }
+            startRow++;
+            if (startRow > endRow || startCol > endCol)
+                break;
+            for (int i = startRow; i <= endRow; i++) {
+                ans.add(matrix[i][endCol]);
+            }
+            endCol--;
+            if (startRow > endRow || startCol > endCol)
+                break;
+            for (int i = endCol; i >= startCol; i--) {
+                ans.add(matrix[endRow][i]);
+            }
+            endRow--;
+            if (startRow > endRow || startCol > endCol)
+                break;
+            for (int i = endRow; i >= startRow; i--) {
+                ans.add(matrix[i][startCol]);
+            }
+            startCol++;
         }
         return ans;
     }
