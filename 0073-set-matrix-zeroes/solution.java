a8f0f160e2547b00104ad002f9d8bb92c30600cc
BHUVESH ROHANKAR <bhuveshrohankar28@gmail.com>
2026-08-25 16:53:04 +0000

    Add solution Runtime - 1 ms (97.41%), Memory - 47.9 MB (5.44%)

--- 0073-set-matrix-zeroes/solution.java
+++ 0073-set-matrix-zeroes/solution.java
@@ -1,24 +1,21 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        int m = matrix.length;
-        int n = matrix[0].length;
+        int colS = matrix[0].length;
+        int rowS = matrix.length;
 
-        boolean[] rows = new boolean[m];
-        boolean[] cols = new boolean[n];
+        boolean[] rows = new boolean[rowS];
+        boolean[] cols = new boolean[colS];
 
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
+        for (int i = 0; i < rowS; i++) {
+            for (int j = 0; j < colS; j++) {
                 if (matrix[i][j] == 0) {
                     rows[i] = true;
                     cols[j] = true;
                 }
             }
-        }
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (rows[i] || cols[j]) {
-                    matrix[i][j] = 0;
-                }
+        }for(int i=0;i<rowS;i++){
+            for (int j = 0; j < colS; j++){
+            if(rows[i]||cols[j]) matrix[i][j]=0;
             }
         }
     }
