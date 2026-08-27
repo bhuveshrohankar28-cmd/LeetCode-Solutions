fe8b02faa8db00054f6aedf21d4a365d931e697e
BHUVESH ROHANKAR <bhuveshrohankar28@gmail.com>
2026-08-27 09:19:23 +0000

    Add solution Runtime - 2 ms (99.95%), Memory - 52 MB (84.56%)

--- 0229-majority-element-ii/solution.java
+++ 0229-majority-element-ii/solution.java
@@ -1,39 +1,41 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        int candidate1 = 0, candidate2 = 0;
         int count1 = 0, count2 = 0;
-
-        for (int num : nums) {
-            if (num == candidate1) count1++;
-            else if (num == candidate2) count2++;
+        int candidate1 = 0, candidate2 = 0;
+        for (int ele : nums) {
+            if (ele == candidate1)
+                count1++;
+            else if (ele == candidate2)
+                count2++;
             else if (count1 == 0) {
-                candidate1 = num;
+                candidate1 = ele;
                 count1 = 1;
-            }
-            else if (count2 == 0) {
-                candidate2 = num;
+            } else if (count2 == 0) {
+                candidate2 = ele;
                 count2 = 1;
-            }
-            else {
+            } else {
                 count1--;
                 count2--;
             }
         }
-
+        /*candidate1 and candidate2 are majority elements 
+        but we still don't know are their count more than n/3 so we need to verify it*/
         count1 = 0;
         count2 = 0;
-
         for (int num : nums) {
-            if (num == candidate1) count1++;
-            else if (num == candidate2) count2++;
+            if (num == candidate1) {
+                count1++;
+            } else if (num == candidate2) {
+                count2++;
+            }
         }
-
-        List<Integer> result = new ArrayList<>();
+        List<Integer> ans = new ArrayList<Integer>();
         int n = nums.length;
+        if (count1 > n / 3)
+            ans.add(candidate1);
+        if (count2 > n / 3)
+            ans.add(candidate2);
 
-        if (count1 > n/3) result.add(candidate1);
-        if (count2 > n/3) result.add(candidate2);
-
-        return result;
+        return ans;
     }
 }
