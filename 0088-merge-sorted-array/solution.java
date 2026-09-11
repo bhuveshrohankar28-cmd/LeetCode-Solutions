d9b1a5618ca2b90b1c25061dba488fd21021e16c
BHUVESH ROHANKAR <bhuveshrohankar28@gmail.com>
2026-09-11 23:53:37 +0000

    Add solution Runtime - 0 ms (100.00%), Memory - 43.5 MB (95.05%)

--- 0088-merge-sorted-array/solution.java
+++ 0088-merge-sorted-array/solution.java
@@ -1,9 +1,15 @@
 class Solution {//again
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int j=0;
-        for(int i=0;i<nums1.length;i++){
-            if(nums1[i]==0&&j<nums2.length) nums1[i]=nums2[j++];
+        int i = m - 1;
+        int j = n - 1;
+        int k = m + n - 1;
 
-        }Arrays.sort(nums1);
+        while (j >= 0) {
+            if (i >= 0 && nums1[i] > nums2[j]) {
+                nums1[k--] = nums1[i--];
+            } else {
+                nums1[k--] = nums2[j--];
+            }
+        }
     }
 }
