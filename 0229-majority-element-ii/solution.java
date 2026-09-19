eff6bb4606c6b985c8497f711b6cb3ac14aef35b
BHUVESH ROHANKAR <bhuveshrohankar28@gmail.com>
2026-09-19 14:33:50 +0000

    Add solution Runtime - 2 ms (99.92%), Memory - 52.6 MB (40.99%)

--- 0229-majority-element-ii/solution.java
+++ 0229-majority-element-ii/solution.java
@@ -1,41 +1,39 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        int count1 = 0, count2 = 0;
-        int candidate1 = 0, candidate2 = 0;
-        for (int ele : nums) {
-            if (ele == candidate1)
+        List<Integer> ans = new ArrayList<Integer>();
+        int n = nums.length;
+        int majority1 = Integer.MIN_VALUE, count1 = 0;
+        int majority2 = Integer.MIN_VALUE, count2 = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == majority1) {
                 count1++;
-            else if (ele == candidate2)
+            } else if (nums[i] == majority2) {
                 count2++;
-            else if (count1 == 0) {
-                candidate1 = ele;
-                count1 = 1;
-            } else if (count2 == 0) {
-                candidate2 = ele;
-                count2 = 1;
+            }else if (count1 <= 0 ) {
+                majority1 = nums[i];
+                count1++;
+            } else if (count2 <= 0) {
+                majority2 = nums[i];
+                count2++;
             } else {
                 count1--;
                 count2--;
             }
         }
-        /*candidate1 and candidate2 are majority elements 
-        but we still don't know are their count more than n/3 so we need to verify it*/
         count1 = 0;
         count2 = 0;
-        for (int num : nums) {
-            if (num == candidate1) {
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == majority1)
                 count1++;
-            } else if (num == candidate2) {
+            if (nums[i] == majority2)
                 count2++;
-            }
         }
-        List<Integer> ans = new ArrayList<Integer>();
-        int n = nums.length;
         if (count1 > n / 3)
-            ans.add(candidate1);
+            ans.add(majority1);
         if (count2 > n / 3)
-            ans.add(candidate2);
-
+            ans.add(majority2);
         return ans;
     }
+
 }
+
